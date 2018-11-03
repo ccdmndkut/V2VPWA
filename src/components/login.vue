06825386e1274152bee8e84ab36afd9193de1d09
Chris <37282688+ccdmndkut@users.noreply.github.com>
2018-11-03 15:33:37 -0400

    working

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,133 +1,189 @@
 <template>
-  <v-content>
-    <v-container fluid fill-height>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4 lg3>
-          <v-card class="elevation-12">
-            <v-toolbar  color="#BBE6FE">
-              <v-toolbar-title>V2V Login</v-toolbar-title>
-              <v-spacer></v-spacer>
-              <img src="https://png.icons8.com/windows/30/password.png">
-            </v-toolbar>
-            <v-card-text class='pb-1'>
-              <v-form>
-                <v-layout justify-space-around="" row wrap>
-                  <v-flex xs1>
-                    <img class='myicon' src="https://png.icons8.com/office/40/ffffff/administrator-male.png">
-                  </v-flex>
-                  <v-flex xs10>
-                    <v-text-field class='ml-2' color="#4B75B2" autofocus autocomplete @keyup.enter="login" v-model="email" required name="login" label="Email" type="email"></v-text-field>
-                  </v-flex>
-                </v-layout>
-                      <v-layout justify-space-around="" row wrap>
-                  <v-flex xs1>
-                   <img class='myicon'  src="https://png.icons8.com/dusk/40/ffffff/lock-2.png">
-                  </v-flex>
-                  <v-flex xs10>
-                     <v-text-field class='ml-2' color="#4B75B2" @keyup.enter="login" autocomplete v-model="mypass" type="password" required name="password" label="Password"></v-text-field>
-                  </v-flex>
-                </v-layout>
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <v-layout justify-space-between class='ml-2 mb-2'>
-                <v-flex xs12>
-                  <v-btn class='mr-2' dark v-on:click="login"  small color="#4B75B2">
-                    Login</v-btn>
-                  <v-btn v-on:click="logout" dark small color="#4B75B2">
-                    Logout</v-btn>
-                </v-flex>
+  <v-content id="body">
+    <div class="login-page">
+      <div class="form">
+        <div class="login-form">
+          <div id="err">{{errM}}</div>
+          <input @focus="errM=''" v-model="email" type="text" placeholder="username" />
+          <input @keydown.enter="login()" @focus="errM=''" v-model="password" type="password" placeholder="password" />
+          <!-- <button @click.prevent="$emit('fire')">login</button> -->
+          <button @click.prevent="login()">login</button>
+          <button @click="logout()">logout</button>
 
-              </v-layout>
-            </v-card-actions>
-            <!-- <v-btn @click="$emit('sentpass', mypass)" small color="primary">
-            Submit</v-btn> -->
-
-          </v-card>
-
-        </v-flex>
-      </v-layout>
-
-    </v-container>
-
+        </div>
+      </div>
+    </div>
   </v-content>
-
 </template>
 
 <script>
-var moment = require("moment");
 import firebase from "firebase";
 export default {
   name: "login",
+  data() {
+    return {
+      user: {},
+      email: "",
+      password: "",
+      errC: "",
+      errM: ""
+    };
+  },
   methods: {
-    logout: function(e) {
+    logout() {
       firebase
         .auth()
         .signOut()
-        .then(
-          user => {
-            alert(user);
-          },
-          err => {
-            alert(err.message);
-          }
-        );
-      e.preventDefault();
+        .then(function() {
+          alert("logged out");
+        })
+        .catch(function(error) {
+          // An error happened.
+        });
     },
-    login: function(e) {
-      var i = this.email;
-      var t = moment().format("h:mm");
+    login() {
       firebase
         .auth()
-        .signInWithEmailAndPassword(i, this.mypass)
-        .then(
-          user => {
-            if (user.user) {
-              window.sessionStorage.setItem("emailForSignIn", i);
-              window.sessionStorage.setItem("logintime", t);
-              console.log(t);
-              this.$emit("fire", i);
-              window.location.reload(true);
-            }
-          },
-          err => {
-            alert(err.message);
-          }
-        );
-      e.preventDefault();
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => this.logmein());
     },
-    onClickButton() {
-      this.$emit("sentpass", this.mypass);
+    logmein() {
+      var user = firebase.auth().currentUser;
+      this.user = user;
+      this.$emit("fire");
     }
   },
-  created() {
-    this.email = window.sessionStorage.getItem("emailForSignIn");
-    var email = this.email;
-    console.log(email);
-    if (!email) {
-      this.$emit("lock");
-    }
-    if (
-      this.email == "chriscombs@vaclaims.net" ||
-      this.email == "denvercombs@vaclaims.net"
-    ) {
-      this.$emit("fire", this.email);
-    }
-  },
-  props: {
-    pass: String
-  },
-  data() {
-    return {
-      myuser: "",
-      email: "",
-      mypass: ""
-    };
-  }
+  computed: {},
+  created() {},
+  props: {}
 };
 </script>
-<style>
-.myicon {
-  padding-top: 10px;
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+#err {
+  position: absolute;
+  top: 235px;
+  color: red;
+}
+#email {
+  position: absolute;
+  top: 250px;
+  color: red;
+}
+.login-page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #ffffff;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 96%;
+  display: block;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.form input:focus {
+  animation: inpa 500ms;
+}
+@keyframes inpa {
+  0%,
+  100% {
+    width: 96%;
+  }
+  50% {
+    width: 100%;
+  }
+}
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4caf50;
+  width: 40%;
+  margin: 5px;
+  border: 0;
+  padding: 15px;
+  color: #ffffff;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,
+.form button:active,
+.form button:focus {
+  background: #43a047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4caf50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before,
+.container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #ef3b3a;
+}
+#body {
+  background: #76b852; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #8dc26f);
+  background: -moz-linear-gradient(right, #76b852, #8dc26f);
+  background: -o-linear-gradient(right, #76b852, #8dc26f);
+  background: linear-gradient(to left, #76b852, #8dc26f);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow: hidden;
 }
 </style>
