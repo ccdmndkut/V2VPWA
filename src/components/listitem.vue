5465f449baac162bdde9aee4b885c3920336182a
Chris <37282688+ccdmndkut@users.noreply.github.com>
2018-11-05 00:12:06 -0500

    loader

---
+++ src/components/listitem.vue
@@ -0,0 +1,127 @@
+<template>
+    <div>
+        <v-card id="card" class='mb-5'>
+            <v-toolbar class='py-1' flat dense color="vuegreendark" dark>
+                <v-layout justify-start>
+                    <v-tooltip bottom>
+                        <v-btn slot="activator" class='mr-0' @click="$emit('pullNote', item, 0)" icon>
+                            <v-icon color="low">looks</v-icon>
+                        </v-btn>
+                        <span>Pull to today</span>
+                    </v-tooltip>
+                    <v-tooltip bottom>
+                        <div slot="activator">
+                            <v-btn class='mr-0' @click="$emit('pullNote', item, 1)" icon>
+                                <v-icon>looks_one</v-icon>
+                            </v-btn>
+                            <v-btn class='mr-0' @click="$emit('pullNote', item, 3)" icon>
+                                <v-icon>looks_3</v-icon>
+                            </v-btn>
+                            <v-btn class='mr-0' @click="$emit('pullNote', item, 5)" icon>
+                                <v-icon>looks_5</v-icon>
+                            </v-btn>
+                        </div>
+                        <span>Move this many days</span>
+                    </v-tooltip>
+                </v-layout>
+                <v-layout justify-center>
+                    <v-toolbar-title>
+                        <v-chip class='elevation-1 px-2 title text-truncate' color="white" text-color="vueblue">
+                            {{item.name}}
+                        </v-chip>
+                    </v-toolbar-title>
+                </v-layout>
+                <v-layout justify-end>
+                    <v-btn @click.stop="$emit('clientInfo', item)" icon>
+                        <v-icon>info</v-icon>
+                    </v-btn>
+                    <v-btn @click="$emit('editNote', item)" icon>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                    <v-tooltip bottom>
+                        <v-btn slot="activator" @dblclick="$emit('completedCheck', item)" icon>
+                            <v-icon color="low">done_outline</v-icon>
+                        </v-btn>
+                        <span>Make Completed</span>
+                    </v-tooltip>
+                    <v-btn @dblclick="$emit('deleteNote', item)" icon>
+                        <v-icon>delete</v-icon>
+                    </v-btn>
+                </v-layout>
+            </v-toolbar>
+            <v-card-text style="text-align: justify; text-indent: 2em;" color="white" class='subheading custcard'>
+                <v-textarea id="mylh" row-height="29" type="text" class="mywrapper" rows="1" auto-grow readonly solo flat color="white" v-model="formatter">
+                </v-textarea>
+            </v-card-text>
+            <v-card-text class="py-0 body-1 font-weight-medium" style="backgroundColor: #CFD8DC ">
+
+                <v-layout class='mt-1'>
+                    <v-flex xs4 text-xs-left>
+                        <v-chip id="chipfontcolor" disabled>{{item.date}}</v-chip>
+                    </v-flex>
+                    <v-flex xs4 text-xs-center>
+                        <v-chip id="chipfontcolor" disabled>{{item.type}}</v-chip>
+                    </v-flex>
+                    <v-flex xs4 text-xs-right>
+                        <v-chip id="chipfontcolor" disabled>
+                            <v-tooltip bottom>
+                                <v-icon id="lh" slot="activator" @click="$emit('priorityUp', item, -1)">keyboard_arrow_up</v-icon>
+                                <span>Priority UP</span>
+                            </v-tooltip>
+                            <span> Priority - {{item.priority}} </span>
+
+                            <v-tooltip bottom>
+                                <v-icon id="lh" slot="activator" @click="$emit('priorityUp', item, 1)">keyboard_arrow_down</v-icon>
+                                <span>Priority DOWN</span>
+                            </v-tooltip>
+
+                        </v-chip>
+
+                    </v-flex>
+                </v-layout>
+            </v-card-text>
+        </v-card>
+    </div>
+
+</template>
+<script>
+export default {
+  name: "listitem",
+  props: ["item", "loading"],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {
+    formatter() {
+      var mynote = this.item.notes;
+      var stringer = JSON.stringify(mynote);
+      return JSON.parse(stringer);
+    }
+  }
+};
+</script>
+<style scoped>
+#card {
+  margin-left: 10%;
+  margin-right: 10%;
+}
+textarea {
+  text-align: justify;
+}
+.custcard {
+  padding-top: 5px;
+  padding-bottom: 0px;
+}
+#mylh {
+  line-height: 20px;
+}
+#chipfontcolor {
+  color: rgba(0, 0, 0, 0.87);
+  background-color: #cfd8dc;
+  margin: 0px;
+}
+#lh {
+  padding-top: 2px;
+}
+</style>
--- src/components/maincont.vue
+++ src/components/maincont.vue
@@ -3,12 +3,12 @@
     <navbar app @logout="logout"> </navbar>
     <v-content>
       <v-container fluid>
+        <div v-if="loading" class="text-xs-center">
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </div>
         <template v-if="!loading">
-          <v-btn @click="switchdb" color="success">text</v-btn>
-          <div v-for="(name, i) in tasks" :key="i">
-            <v-chip @click="tasks.pop()">
-              {{i +1}}. {{name.name}}
-            </v-chip>
+          <div v-for="(item, i) in db.tasks" :key="i">
+            <listitem :loading="loading" :item="item"></listitem>
           </div>
         </template>
       </v-container>
@@ -19,76 +19,66 @@
 <script>
 import firebase from "firebase";
 import firestore from "./firebaseInit";
-
 import navbar from "./navbar";
+import listitem from "./listitem";
 
 export default {
   name: "maincont",
   components: {
-    navbar
+    navbar,
+    listitem
   },
   props: ["user"],
   data() {
     return {
       myemail: this.user.email,
-      loading: true,
-      tasks: "",
-      db: "trash"
+      loading: Boolean,
+      db: {
+        tasks: ""
+      }
     };
   },
   methods: {
     fs(i) {
       this.loading = true;
-      this.$binding("users", firebase.firestore().collection(i)).then(users => {
+      this.$binding("tasks", firebase.firestore().collection(i)).then(tasks => {
         console.log("got db"); // => __ob__: Observer
-        this.loading = false;
         // this.$emit("loadingTrigger", false);
-        this.tasks = users;
+        this.db.tasks = tasks;
+        this.loaded();
       });
     },
-    switchdb() {
-      this.loading = true;
-
-      if (this.db == "trash") {
-        this.db = "clients";
-      } else {
-        this.db = "trash";
-      }
-      this.fs(this.db);
+    loaded() {
+      this.loading = false;
     },
     logout() {
       this.$emit("logout");
     }
   },
   computed: {
-    userLength() {
-      var u = this.user;
-      if (this.user) {
-        return Object.keys(u);
-      } else {
-        return 0;
+    dblength() {
+      return this.tasks.length;
+    },
+    userdbdef() {
+      let user = this.user.email;
+      if (user == "chriscombs@vaclaims.net") {
+        return {
+          tasks: "tasks",
+          client: "clients",
+          trash: "trash"
+        };
+      } else if (user == "denvercombs@vaclaims.net") {
+        return {
+          tasks: "denver",
+          client: "clientsdlc",
+          trash: "trash"
+        };
       }
     }
   },
-  // firestore() {
-  //   return {
-  //     persons: {
-  //       // collection reference.
-  //       ref: firestore.collection(this.db),
-  //       // Bind the collection as an object if you would like to.
-  //       objects: false,
-  //       resolve: data => {
-  //         this.tasks = data;
-  //         this.loading = false;
-  //       },
-  //       reject: err => {
-  //         console.log(err);
-  //       }
-  //     }
-  //   };
-  // }
   mounted() {
-    this.fs("clients");
+    var db = this.userdbdef.trash;
+    this.fs(db);
   }
 };
 </script>
