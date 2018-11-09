0b8f41f1e23375e3dd8cc1b7df60f07b4819d791
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-11-08 22:47:14 -0500

    pose and subpage

---
+++ src/subpage/subapp.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="body">
+    <v-content>
+      <v-container>
+        <v-layout justify-center>
+          <v-flex justify-center=text-xs-center xs5>
+            <Box class="box" :pose="isVisible ? 'visible' : 'hidden'" />
+            <v-alert id="warning" type="warning" :value="true" class="display-1">
+              You should not be here. Leave NOW!!!
+            </v-alert>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import posed from "vue-pose";
+export default {
+  name: "subapp",
+  components: {
+    Box: posed.div({
+      visible: {
+        opacity: 1,
+        transition: { duration: 20 }
+      },
+      hidden: { opacity: 0 }
+    })
+  },
+
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {
+    setInterval(() => {
+      this.isVisible = !this.isVisible;
+    }, 1000);
+  }
+};
+</script>
+<style>
+body {
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  background: #54e365;
+}
+#warning {
+  color: black;
+}
+.bs::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    pages: {
-      index: {
-        entry: "src/index/main.js",
-        template: "public/index.html",
-        filename: "index.html",
-        // when using the entry-only string format,
-        // template is inferred to be `public/subpage.html`
-        // and falls back to `public/index.html` if not found.
-        // Output filename is inferred to be `subpage.html`.
-        subpage: "src/subpage/main.js"
-      }
-    };
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html"
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    subpage: "src/subpage/main.js"
+  }
+};
