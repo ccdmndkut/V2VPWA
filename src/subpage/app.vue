9284b82ab7e12e398ad08cd9da35ae8437fbc5e2
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-11-08 21:31:21 -0500

    d

---
+++ src/subpage/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <v-btn color="success">hi world</v-btn>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created() {}
+};
+</script>
+<style>
+.bs::-webkit-scrollbar {
+  display: none;
+}
+</style>
